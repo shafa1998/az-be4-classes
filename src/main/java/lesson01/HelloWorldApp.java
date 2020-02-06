b0cb464d4b38616ed3a68929c2e74f1745345acd
Alexey <alexey.rykhalskiy@gmail.com>
2020-02-06 13:35:30 +0400

    -- calc

--- src/main/java/lesson01/HelloWorldApp.java
+++ src/main/java/lesson01/HelloWorldApp.java
@@ -1,8 +1,19 @@
 package lesson01;
 
+import java.util.Scanner;
+
 public class HelloWorldApp {
   public static void main(String[] args) {
-    System.out.println("Hello!");
-    System.out.println("from Java!");
+    // 1
+    Scanner in = new Scanner(System.in);
+    System.out.print("Enter the number a:");
+    int a = in.nextInt();
+    System.out.print("Enter the number b:");
+    int b = in.nextInt();
+
+    // 2
+    int c = a + b;
+    // 3
+    System.out.printf("The sum is: %d\n", c);
   }
 }
