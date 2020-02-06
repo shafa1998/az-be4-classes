11128d4ff556ec07dfbdd862764937bc6de7501a
Alexey <alexey.rykhalskiy@gmail.com>
2020-02-06 16:30:17 +0400

    -- ControlStructures.java

--- src/main/java/lesson01/HelloWorldApp.java
+++ src/main/java/lesson01/HelloWorldApp.java
@@ -3,6 +3,9 @@ package lesson01;
 import java.util.Scanner;
 
 public class HelloWorldApp {
+
+  static int k;
+
   public static void main(String[] args) {
     // 1
     Scanner in = new Scanner(System.in);
@@ -13,7 +16,18 @@ public class HelloWorldApp {
 
     // 2
     int c = a + b;
+
     // 3
     System.out.printf("The sum is: %d\n", c);
+
+    // declaration
+    int x;
+//    x = 33;
+//    int y = 66;
+//    System.out.println(x);
+    System.out.println(k);
+
+
+
   }
 }
