10f41a44051eb536b1ea73e03cbb88a9d01fc1fe
Alexey <alexey.rykhalskiy@gmail.com>
2020-02-06 11:51:41 +0400

    -- 1st

--- src/main/java/lesson01/HelloWorldApp.java
+++ src/main/java/lesson01/HelloWorldApp.java
@@ -2,6 +2,7 @@ package lesson01;
 
 public class HelloWorldApp {
   public static void main(String[] args) {
-    System.out.println("Hello from Java!");
+    System.out.println("Hello!");
+    System.out.println("from Java!");
   }
 }
